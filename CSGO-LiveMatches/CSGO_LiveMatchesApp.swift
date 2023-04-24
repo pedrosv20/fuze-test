b945b276cd1ddbeb440cffd4aac0e58088381833
Pedro <pedro1silva1vargas@gmail.com>
2023-04-24 14:15:19 -0300

    Create workspace with project and modules package

---
+++ CSGO-LiveMatches/CSGO_LiveMatchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSGO_LiveMatchesApp.swift
+//  CSGO-LiveMatches
+//
+//  Created by Pedro Vargas on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CSGO_LiveMatchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
